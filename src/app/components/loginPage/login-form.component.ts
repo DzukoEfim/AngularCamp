afff7bf3e29a88503bf78b40224c6a448439504f
Work <Work>
2017-03-19 13:42:36 +0300

    19.03.2017 added support of Enter button on login page

--- src/app/components/loginPage/login-form.component.ts
+++ src/app/components/loginPage/login-form.component.ts
@@ -10,13 +10,22 @@ import { LoginService } from '../../shared/services/login.service';
 export class LoginFormComponent {
     public userName: string = '';
     public password: string = '';
+    public formValid: boolean = true;
 
     constructor(private loginService: LoginService) {
         this.loginService = loginService;
     }
 
-    public logInUser () {
+    public logInUser (): void {
         this.loginService.login(this.userName, this.password);
     };
+
+    public onEnter(): void {
+        if (this.userName.length === 0 || this.password.length === 0) {
+            this.formValid = false;
+            return;
+        }
+        this.logInUser();
+    }
 
 }
